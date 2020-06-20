93b533543ba4ca0f427a8b1a71d66b5070dbf593
Oleksii Pokhil <AlekcukP@gmail.com>
2020-06-20 14:09:44 +0300

    Fixed bugs. Make code shorter

--- project13/script.js
+++ project13/script.js
@@ -1,25 +1,22 @@
-
-
-
 class DoList {
 
     static STATUS_DONE = 'dolist_done';
     static STATUS_DEAFAULT = 'dolist_default';
-    static ATTRIBUTE_NAME = 'DoListId';
+    static ATTRIBUTE_NAME = 'data-dolist-id';
 
     
     
     constructor(arr){
-    this.toDoTasks = arr;
-    this.container = document.getElementById('container');
-    this.toDoList = this.container.querySelector('.dolist_body');
-    this.inputForm = this.container.querySelector('.dolist_input');
-    this.addBtn = this.container.querySelector('.dolist_btn');
-    
-    this.createInitList();
+        this.toDoTasks = arr;
+        this.container = document.getElementById('container');
+        this.toDoList = this.container.querySelector('.dolist_body');
+        this.inputForm = this.container.querySelector('.dolist_input');
+        this.addBtn = this.container.querySelector('.dolist_btn');
+        
+        this.createInitList();
 
-    this.toDoList.addEventListener('click', this.onTaskListClick.bind(this));
-    this.addBtn.addEventListener('click', this.onAddbtnClick.bind(this));
+        this.toDoList.addEventListener('click', this.onTaskListClick.bind(this));
+        this.addBtn.addEventListener('click', this.onAddbtnClick.bind(this));
     }
 
    static createTask(taskText){
@@ -28,28 +25,9 @@ class DoList {
         newTask.className = DoList.STATUS_DEAFAULT;
         return newTask;
     }
-    
-    static initSetStatus(el, condition){
-        if(condition){
-            el.classList.add(DoList.STATUS_DONE);
-        } else{
-            el.classList.remove(DoList.STATUS_DONE);
-        }
 
-    return el;
-    }
-    
-    static setElAttribute(el, attrName, attrValue){
-        el.setAttribute(`${attrName}`, attrValue);
+    static setStatus(el, condition){
 
-    return el;
-    }
-    
-    static appendElement(el, container){
-        container.append(el);
-    }
-
-    static setSataus(el, condition){
         if(condition){
             el.classList.add(DoList.STATUS_DONE);
         } else{
@@ -58,42 +36,35 @@ class DoList {
     }
 
     createInitList(){
-        let length = this.toDoTasks.length;
-
-        for(let i = 0; i < length; i++){
-            this.createListEl(i);
-        }
+        this.toDoTasks.forEach((item, index) => this.createListEl(index));
     }
 
     createListEl(i){
         let listEl = DoList.createTask(this.toDoTasks[i].title);
-            listEl = DoList.initSetStatus(listEl, this.toDoTasks[i].completed);
-            listEl = DoList.setElAttribute(listEl, DoList.ATTRIBUTE_NAME, this.toDoTasks[i].id);
-            DoList.appendElement(listEl, this.toDoList);
+        listEl.setAttribute(DoList.ATTRIBUTE_NAME, this.toDoTasks[i].id);
+        DoList.setStatus(listEl, this.toDoTasks[i].completed);
+        this.toDoList.append(listEl);
     }
 
     onTaskListClick(e){
         if(e.target.classList.contains(DoList.STATUS_DEAFAULT)){
-            this.findEl(e.target.getAttribute(DoList.ATTRIBUTE_NAME), e.target);
-            
+
+            let index = e.target.getAttribute(DoList.ATTRIBUTE_NAME);
+            let arrayTarget = this.toDoTasks.find((item) => item.id == index);      
+                
+            this.changeComplete(arrayTarget);
+            DoList.setStatus(e.target, arrayTarget.completed);
         }
     }
 
-    findEl(index, el){
-        this.toDoTasks.find(function(item){
-            
-            if(item.id == index){
-                if(item.completed){
-                    item.completed = false;
-                    console.log(item);
-                } else{
-                    item.completed = true;
-                    console.log(item);
-                }
-            }
-            DoList.setSataus(el, item.completed);
-        }); 
-        
+
+    changeComplete(item){
+        if(item.completed){
+        item.completed = false;
+
+        } else{
+        item.completed = true;
+    }
     }
 
     onAddbtnClick(){
@@ -116,9 +87,12 @@ class DoList {
         return res;
     }
 
-
-
-
 }   
+
+
+
+
+
+
 fetch('http://jsonplaceholder.typicode.com/todos').then(res => res.json()).then(data => new DoList(data));
     
