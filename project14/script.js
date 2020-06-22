94fd4287b8fcca7b313eb1cfd1d3d6d1458c4988
Oleksii Pokhil <AlekcukP@gmail.com>
2020-06-22 14:44:19 +0300

    Fixed bugs.

--- project14/script.js
+++ project14/script.js
@@ -1,14 +1,1 @@
-async function getLists (){
-
-    let photosResponse = await fetch('https://jsonplaceholder.typicode.com/photos');
-    let photosList = await photosResponse.json();
-    
-    
-    let albumsResponse = await fetch('https://jsonplaceholder.typicode.com/albums');
-    let albumsList = await albumsResponse.json();
-    
-    new Gallery(albumsList, photosList);
-
-}
-
-getLists();
+fetch('https://jsonplaceholder.typicode.com/albums').then(res => res.json()).then(data => new Gallery(data));
